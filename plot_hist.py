23c10d293e7d148c8ff43c13f2a25d0722a83714
Simone Longo <simone.longo@gmail.com>
2020-04-10 09:30:29 -0600

    added  descriptive stats and nbins argument to command  line. docs still need updating

--- plot_hist.py
+++ plot_hist.py
@@ -5,6 +5,15 @@ import seaborn as sns
 from collections import defaultdict
 
 cols = defaultdict(list)
+nbins = None
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == '-n':
+        try:
+            nbins = int(sys.argv[2])
+        except (IndexError, ValueError):
+            print('ERROR: \'-n\' must be a positive integer. Example use:\n\t\t$ cat numbers.txt | histogram  -n 25')
+            exit(1)
 
 for line in sys.stdin:
     sp = line.split('\t')
@@ -15,6 +24,8 @@ for line in sys.stdin:
             continue
 
 for array in cols.values():
-    sns.distplot(np.array(array))
+    arr = np.array(array)
+    sns.distplot(arr, bins=nbins, label='Mean:    {:.4E}\nMedian: {:.4E}\nRange:  [{:.4E}, {:.4E}]'.format(np.mean(arr), np.median(arr), np.min(arr), np.max(arr)))
 
+plt.legend()
 plt.show()
